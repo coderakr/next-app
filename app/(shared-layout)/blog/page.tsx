8052b9c69e1e9906924572651dc023a7b52e143c
coderakr <139627558+coderakr@users.noreply.github.com>
2026-04-05 23:35:16 +0530

    added useCache

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,6 +4,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Metadata } from "next";
+import { cacheLife } from "next/dist/server/use-cache/cache-life";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 import Image from "next/image";
 import Link from "next/link";
 import { connection } from "next/server";
@@ -37,11 +39,11 @@ export default function BlogPage() {
 }
 
 async function BlogList() {
-  // "use cache";
-  // cacheLife("hours");
-  // cacheTag("blog");
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blog");
 
-  await connection();
+  // await connection();
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
